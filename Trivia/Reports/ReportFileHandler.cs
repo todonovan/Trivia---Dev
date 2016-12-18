3af2093ae6fdbfeea6e603d0402ca578e3453ba8
Terrence <terrence.odonovan@gmail.com>
2016-12-17 23:45:37 -0500

    Small fixes to reports view; skeleton file handling code added

--- Trivia/Reports/ReportFileHandler.cs
+++ Trivia/Reports/ReportFileHandler.cs
@@ -1,22 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Trivia.Reports
 {
     public class ReportFileHandler
     {
+        private Excel.Application _excelApp;
+
+        public ReportFileHandler()
+        {
+            _excelApp = new Excel.Application();
+        }
+
         public void OpenReport(string fileName)
         {
             string fullName = ConfigurationManager.AppSettings["report_save_config"].ToString() + fileName;
+            try
+            {
+                Excel.Workbook workbook = _excelApp.Workbooks.Open(fullName);
+                _excelApp.Visible = true;
+            }
+            catch
+            {
+
+            }
         }
 
         public void DeleteReport(string fileName)
         {
-            string fullName = ConfigurationManager.AppSettings["report_save_config"].ToString() + fileName;
+            string dirName = ConfigurationManager.AppSettings["report_save_config"].ToString();
+            File.Delete(dirName + fileName);            
         }
     }
 }
