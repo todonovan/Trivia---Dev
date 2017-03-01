36d1f6ce3bd1fbf2ac45d5eb98bc242fe13f5227
Terrence <terrence.odonovan@gmail.com>
2017-02-28 21:02:02 -0500

    Initial wiring up of view <--> viewmodels; main control panel functionality

--- Thermometer/MainWindowViewModel.cs
+++ Thermometer/MainWindowViewModel.cs
@@ -3,21 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Thermometer
 {
     public class MainWindowViewModel : BindableBase
     {
+        private int _maxValue;
+        public int MaxValue
+        {
+            get { return _maxValue; }
+            set { SetProperty(ref _maxValue, value); }
+        }
+
+        private int _currentValue;
+        public int CurrentValue
+        {
+            get { return _currentValue; }
+            set { SetProperty(ref _currentValue, value); }
+        }
+
         public MainWindowViewModel()
         {
             DisplayThermometerCommand = new RelayCommand(OnDisplayThermometer);
+            ResetCommand = new RelayCommand(OnReset);
         }
 
         private void OnDisplayThermometer()
         {
+            ThermometerWindowViewModel thermViewModel = new ThermometerWindowViewModel();
+            Window thermWindow = new ThermometerWindow();
 
+            thermViewModel.InitializeParameters(MaxValue, CurrentValue);
+
+            thermWindow.DataContext = thermViewModel;
+            thermWindow.Show();
+        }
+
+        private void OnReset()
+        {
+            MaxValue = 0;
+            CurrentValue = 0;
         }
 
         public RelayCommand DisplayThermometerCommand { get; private set; }
+        public RelayCommand ResetCommand { get; private set; }
     }
 }
---
+++ Thermometer/ThermometerWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thermometer
+{
+    public class ThermometerWindowViewModel : BindableBase
+    {
+        private int _maxValue;
+        public int MaxValue
+        {
+            get { return _maxValue; }
+            set { SetProperty(ref _maxValue, value); }
+        }
+
+        private int _currentValue;
+        public int CurrentValue
+        {
+            get { return _currentValue; }
+            set { SetProperty(ref _currentValue, value); }
+        }
+
+
+
+        public void InitializeParameters(int maxValue, int currentValue)
+        {
+            MaxValue = maxValue;
+            CurrentValue = currentValue;
+
+        }
+    }
+}
